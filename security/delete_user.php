db669e072ed826feee3dc80e95d674a5885c44a9
doicobaonhieungayvui <huynhhuy283@gmail.com>
2024-10-01 10:39:53 +0700

    Lab 3

---
+++ security/delete_user.php
@@ -0,0 +1,32 @@
+
+<?php
+session_start();
+require_once 'models/UserModel.php';
+$userModel = new UserModel();
+
+// Kiểm tra xem người dùng có đang đăng nhập không
+if (!isset($_SESSION['id'])) {
+    die("Bạn cần đăng nhập để thực hiện hành động này.");
+}
+
+// Kiểm tra ID người dùng muốn xóa
+if (empty($_GET['id'])) {
+    die("ID không hợp lệ.");
+}
+
+$userIdToDelete = $_GET['id'];
+$currentUserId = $_SESSION['id'];
+
+// Kiểm tra xem người dùng hiện tại có quyền xóa người dùng này không
+if ($currentUserId != $userIdToDelete) {
+    $_SESSION['message'] = "Bạn không có quyền xóa người dùng khác.";
+    header('location: list_users.php');
+    exit();
+}
+
+// Thực hiện hành động xóa
+$userModel->deleteUserById($userIdToDelete);
+$_SESSION['message'] = "Bạn đã xóa thành công.";
+header('location: list_users.php');
+
+?>
